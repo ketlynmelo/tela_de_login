f08b90da3f2902a4639e23b10a939d66112d71a8
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-04-27 08:01:09 -0300

    Docs - Inicializacao do site na parte de documentacao e formulario

---
+++ script.js
@@ -0,0 +1,20 @@
+let form = document.getElementById('formulario');
+
+let dados = [];
+
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+
+        let valido = true;
+
+        let nome = document.getElementById('nome').value;
+        let senha = document.getElementById('senha').value;
+
+        document.getElementById('invalid-feedback').textContent = '';
+
+
+
+
+})
