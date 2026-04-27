ccd848ec2dd0aca68b5d9ac0d4350c9da1ca99ad
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-04-27 08:11:08 -0300

    ADD - Comeco da validacao no java

--- script.js
+++ script.js
@@ -11,10 +11,25 @@ form.addEventListener('submit', function (e) {
 
         let nome = document.getElementById('nome').value;
         let senha = document.getElementById('senha').value;
+        const feedback = document.getElementById('invalid-feedback');
+
+       
+        i f (nomeInput.value.trim() === '') {
+         feedback.textContent = 'O nome é obrigatório!';
+        } else {
+         feedback.textContent = ''
+        alert('Formulário enviado com sucesso!');
 
         document.getElementById('invalid-feedback').textContent = '';
 
+    if (!nome.includes(" ")) {
+
+        document.getElementById('erroNome').textContent = 'Nome inválido,deve conter o nome completo';
+        valido = false;
+    }
 
 
+        form.reset();
+    
 
 })
